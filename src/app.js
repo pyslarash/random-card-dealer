cb2da7cf04b3add6bb03550339bd30048ef9540b
pyslarash <114081632+pyslarash@users.noreply.github.com>
2023-01-01 22:07:39 -0500

    Finished Work

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector('.card');
+  document.querySelector('#width').addEventListener("keypress", function (e) {  // Changing width of the card
+    if (e.key === 'Enter') {
+      var inputWidth = document.querySelector('#width').value;
+      card.style.width = inputWidth + "px";
+      console.log(inputWidth);
+    }
+  });
+  document.querySelector('#height').addEventListener("keypress", function (e) {  // Changing height of the card
+    if (e.key === 'Enter') {
+      var inputHeight = document.querySelector('#height').value;
+      card.style.height = inputHeight + "px";
+    }
+  });
+  document.querySelector('.top-suit').innerHTML = generateRandomSuit(); // We are changing the top suit
+  document.querySelector('.bottom-suit').innerHTML = document.querySelector('.top-suit').innerHTML; // The bottom suit should be the same
+  document.querySelector('.number').innerHTML = generateRandomNumber(); // Changing the number
+  let counter = 10;
+  setInterval(function(){
+    document.querySelector('#timer').innerHTML = counter;
+    counter--;
+    if (counter === 0) {
+      window.location.reload();
+    }
+  }, 1000);
+  // document.querySelector('#timer').innerHTML = countDown();
 };
+
+let generateRandomNumber = () => {                                  // This function generates a random number.
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let indexNum = Math.floor(Math.random()*numbers.length);
+  return numbers[indexNum];
+}
+
+let generateRandomSuit = () => {                                    // This function generates a random suit and then picks the color of it in certain instances.
+  let suit = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+  let indexSuit = Math.floor(Math.random()*suit.length);
+  if (indexSuit === 2 || indexSuit === 3) {
+    document.querySelector('.top-suit').style.color = "red";
+    document.querySelector('.bottom-suit').style.color = "red";
+  }
+  return suit[indexSuit];
+}
